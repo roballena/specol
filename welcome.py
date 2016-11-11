3fe76284e28c858df8d4020f42d082c9924d4563
ccatapan <carlo.catapang@nokia.com>
2016-11-11 18:15:39 +0800

    test

--- welcome.py
+++ welcome.py
@@ -23,7 +23,7 @@ def Welcome():
 
 @app.route('/myapp')
 def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix fuck!'
+    return 'Welcome again to my app running on Bluemix fuck test!'
 
 @app.route('/api/people')
 def GetPeople():
