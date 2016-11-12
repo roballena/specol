6a8a8a80a604beb06eb740584fbb56a3875cdbb4
ccatapan <carlo.catapang@nokia.com>
2016-11-12 10:20:45 +0800

    Added prints

--- compiler.py
+++ compiler.py
@@ -2,7 +2,11 @@ import os
 
 
 def compileBinary():
+    print "Compiler c++ code\n"
     os.system('g++ main.cpp -o main')
+    print 'Code compilation done!\n'
     
 def runBinary():
+    print 'Running main.exe'
     os.system('main.exe')
+    print 'Program execution done.\n'
